df6e43e690e9ff1cc9872420233adb89aaa2870b
lucasweiby <lucas_weiby@hotmail.com>
2019-01-09 20:07:35 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<?php include 'conf/head.php' ?>
+
+<body>
+  <?php include 'views/nav.php' ?>
+
+  <?php include 'views/header.php' ?>
+
+  <?php include 'conf/scripts.php' ?>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,11 @@
+<header class="cabecalho">
+    <div class="row">
+      <div class="col-md-6 logo"><img src="img/logo-topo.png" alt="Sérgio Rebouças" class="logo-img"></div>
+      <div class="col-md-6">
+        <div class="links">
+          <a href="#">LEIA MEU LIVRO</a>
+          <a href="#">CADASTRE-SE</a>
+        </div>
+      </div>
+    </div>
+</header>
