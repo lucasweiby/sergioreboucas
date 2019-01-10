ade1e9dabe9abfb9b0d825ee77960a8ae9099332
lucasweiby <lucas_weiby@hotmail.com>
2019-01-10 14:29:07 -0300

    Header

--- views/header.php
+++ views/header.php
@@ -3,9 +3,19 @@
       <div class="col-md-6 logo"><img src="img/logo-topo.png" alt="Sérgio Rebouças" class="logo-img"></div>
       <div class="col-md-6">
         <div class="links">
-          <a href="#">LEIA MEU LIVRO</a>
-          <a href="#">CADASTRE-SE</a>
+          <a href="#" class="main-link">LEIA MEU LIVRO</a>
+          <a href="#" class="main-link">CADASTRE-SE</a>
+        </div>
+        <div class="sociais">
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
         </div>
       </div>
+    </div>
+    <div class="pic1">
+      <img class="pic-header" src="img/SERGIOREBOUCASfotoHome.png" alt="">
     </div>
 </header>
